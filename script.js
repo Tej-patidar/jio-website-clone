e64357484bee7765c5a35da1dad6b5c2ce0907da
Tarun <tarunpatidar742@gmail.com>
2023-04-02 22:29:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// $('.icon').slick();
+
+
+
+$('.icon').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots:true,
+  });
+
+
+//   $('.shadow').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
